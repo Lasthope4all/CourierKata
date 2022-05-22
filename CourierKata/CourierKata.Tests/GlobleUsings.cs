1efecc1f9d540aab517625854b8df4d78882ee66
Owen Bayley <ojbayley@gmail.com>
2022-05-22 12:58:23 +0100

    Added in base tests for parcel creation

---
+++ CourierKata/CourierKata.Tests/GlobleUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Courier.Core.Enums;
+global using Courier.Core.Models;
+global using FluentAssertions;
+global using System;
+global using System.Collections.Generic;
+global using Xunit;
